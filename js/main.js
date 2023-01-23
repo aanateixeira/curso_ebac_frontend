b09be03deb6ea0a148fe24e592fab5c9c5266427
Ana Teixeira <114105288+aanateixeira@users.noreply.github.com>
2023-01-23 14:30:32 -0300

    Adicionando exercicio jquery
    adicionando exercicio plugins jquery

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+
+    $('#cpf').mask('000.000.000-00', {
+        placeholder: '___.___.___-__'
+    })
+
+    $('#cep').mask('00000-000', {
+        placeholder: '_____-___'
+    })
+
+    }); 

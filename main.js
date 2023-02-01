bf94e5dbc34437030bd8257bd6b15581180df11f
Ana Teixeira <114105288+aanateixeira@users.noreply.github.com>
2023-01-31 23:23:16 -0300

    subindo exercicio
    subindo exercicio html_js contador de numeros

---
+++ main.js
@@ -0,0 +1,34 @@
+const form = document.getElementById('form-checar');
+const primeiroNumero = document.getElementById('primeiro-numero');
+const segundoNumero = document.getElementById('segundo-numero');
+
+    const mensagemSucesso = 'Sim! O 2° número é maior que o 1° número :)'
+    const mensagemErro = 'Não! O 2° número é menor que o 1° número :('
+
+function checarNumero() {
+    mensagemSucesso.value = ''
+    mensagemErro.value = ''
+
+    if (primeiroNumero.value < segundoNumero.value) {
+            const containerMensagemErro = document.querySelector('.error-message');
+            containerMensagemErro.innerHTML = "";
+            containerMensagemErro.style.display = 'none';
+            
+            const containerMensagemSucesso = document.querySelector('.sucess-message');
+            containerMensagemSucesso.innerHTML = mensagemSucesso;
+            containerMensagemSucesso.style.display = 'block';
+        } else {
+            const containerMensagemSucesso = document.querySelector('.sucess-message');
+            containerMensagemSucesso.innerHTML = "";
+            containerMensagemSucesso.style.display = 'none';
+            
+            const containerMensagemErro = document.querySelector('.error-message');
+            containerMensagemErro.innerHTML = mensagemErro;
+            containerMensagemErro.style.display = 'block';
+        }  
+};
+
+form.addEventListener('submit', function(e) {    
+    e.preventDefault();
+    return checarNumero();
+});
